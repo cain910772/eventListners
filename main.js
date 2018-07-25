da91ec1aea3f708f1581116e86acb0a7991121ff
cain910772 <cain910772@yahoo.com>
2018-07-25 13:57:49 -0500

    first

---
+++ main.js
@@ -0,0 +1,53 @@
+// let myToggle = (document.querySelectorAll('.Buttons'), function(el) {
+//     // classList supports 'contains', 'add', 'remove', and 'toggle'
+//     el.classList.toggle('activate-flight');
+//   });
+
+//   myToggle()
+
+
+
+
+let myGet = document.getElementById("activate-flight").addEventListener("click" , function(e) {
+    document.getElementById("flight").innerHTML ="FLIGHT POWER ACTIVATED" })
+
+    let myGet2 = document.getElementById("activate-mindreading").addEventListener("click" , function(e) {
+        document.getElementById("mindreading").innerHTML ="MINDREADING POWER ACTIVATED" })
+
+        let myGet3 = document.getElementById("activate-xray").addEventListener("click" , function(e) {
+            document.getElementById("xray").innerHTML ="X-RAY POWER ACTIVATED" })
+
+            let myGetet4 = document.getElementById("activate-all").addEventListener("click",function(){
+                alert("ALL POWERS ARE GO!!!!")
+            })
+
+            let myGetet5 = document.getElementById("deactivate-all").addEventListener("click",function(){
+                alert("ALL POWERS ARE OFF, PUT YOUR GLASSES BACK ON SUPERMAN!!!!!!")
+            })
+             
+// let myGet6=document.querySelectorAll("power-disabled")
+//     if(myGet === 'click')
+//     alert("ALL POWERS ARE GO!!!!")
+
+             
+
+
+
+
+
+
+
+
+
+/*
+    Now write two more event handlers that activate the other two powers
+    when the corresponding buttons are clicked.
+*/
+
+
+/*
+    Write two more event handlers for activating and deactivating all powers
+    when the corresponding buttons are clicked. You will need to use the
+    `document.querySelectorAll()` method for these.
+*/
+
